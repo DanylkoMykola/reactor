fbefeb96731338be642e39af30a4438c0168c92b
mdanylk <mdanylko@agileassets.com>
2025-09-12 13:26:45 +0300

    Update Demo class

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -11,9 +11,9 @@ public class Demo {
     public static void main(String[] args) {
         IterablePublisher<Integer> iterablePublisher = new IterablePublisher<>(List.of(1, 2, 3, 4, 5));
 
-/*        Publisher<Integer> filtered = new FilterPublisher<>(iterablePublisher, i -> i % 2 == 0);
+        Publisher<Integer> filtered = new FilterPublisher<>(iterablePublisher, i -> i % 2 == 0);
         Publisher<String> mapped = new MapPublisher<>(filtered, i -> "Number " + i);
-        mapped.subscribe(new PrintSubscriber<>());*/
+        mapped.subscribe(new PrintSubscriber<>());
 
         TakePublisher<Integer> takePublisher = new TakePublisher<>(iterablePublisher, 3);
         takePublisher.subscribe(new PrintSubscriber<>());
